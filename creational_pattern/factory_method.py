a909dfc222bafeb6033146700d03b0aeeec680ad
incCool <phr_sdu@163.com>
2019-09-02 18:18:51 +0800

    code

--- creational_pattern/factory_method.py
+++ creational_pattern/factory_method.py
@@ -13,3 +13,32 @@ When a class wants to specify the object it creates by its subclasses.
 When the class delegates the responsibility of creating an object to one of several help subclasses,
 and you want to localize which help subclass is the agent.
 '''
+
+class Boy:
+
+    def __init__(self):
+        self.trans = dict(boy=u"男生",girl=u"女孩")
+
+    def get(self, F_OR_M):
+        '''logic process'''
+        return self.trans[F_OR_M]
+
+
+class Girl:
+    """Simply echoes the msg ids"""
+    def __init__(self):
+        self.trans = dict(boy=u"男生",girl=u"女孩")
+    def get(self, F_OR_M):
+        return self.trans[F_OR_M]
+
+
+def FactoryMethod(F_M="Boy"):
+    """ return distinct string with distinct parameter """
+    f_m = dict(Boy=Boy, Girl=Girl)
+    return f_m[F_M]()
+
+
+# Create our localizers
+e, g = FactoryMethod("Boy"), FactoryMethod("Girl")
+
+print(g.get('girl'))
