b8edddae7fad5244107dbb9a694cd997dc960e3a
incCool <phr_sdu@163.com>
2019-09-02 23:15:04 +0800

    Person -> boy or girl -> Factory

--- creational_pattern/factory_method.py
+++ creational_pattern/factory_method.py
@@ -14,31 +14,49 @@ When the class delegates the responsibility of creating an object to one of seve
 and you want to localize which help subclass is the agent.
 '''
 
-class Boy:
 
-    def __init__(self):
-        self.trans = dict(boy=u"男生",girl=u"女孩")
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
 
-    def get(self, F_OR_M):
-        '''logic process'''
-        return self.trans[F_OR_M]
-
-
-class Girl:
-    """Simply echoes the msg ids"""
-    def __init__(self):
-        self.trans = dict(boy=u"男生",girl=u"女孩")
-    def get(self, F_OR_M):
-        return self.trans[F_OR_M]
+    def printPerson(self):
+        pass
 
 
-def FactoryMethod(F_M="Boy"):
-    """ return distinct string with distinct parameter """
-    f_m = dict(Boy=Boy, Girl=Girl)
-    return f_m[F_M]()
+class Factory:
+
+    def FactoryMethod(F_M='F', name='heihei', age=100):
+        """ return distinct string with distinct parameter """
+        if F_M == 'F':
+            return Boy(name, age)
+        elif F_M == 'M':
+            return Girl(name, age)
 
 
-# Create our localizers
-e, g = FactoryMethod("Boy"), FactoryMethod("Girl")
+class Boy(Person):
 
-print(g.get('girl'))
+    def __init__(self, name, age):
+        super().__init__(name, age)
+        self.s1 = "Manhood"
+
+    def printPerson(self):
+        print(self.name + ' ' + str(self.age) + ' ' + self.s1)
+
+
+class Girl(Person):
+    """Girl class"""
+
+    def __init__(self, name, age):
+        super().__init__(name, age)
+        self.s2 = "Half Sky"
+
+    def printPerson(self):
+        print(self.name + ' ' + str(self.age) + ' ' + self.s2)
+
+
+boy = Factory.FactoryMethod('M', 'inc', 12)
+girl = Factory.FactoryMethod('F', 'rose', 13)
+
+boy.printPerson()
+girl.printPerson()
