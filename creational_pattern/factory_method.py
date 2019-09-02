cd54f5f2b8e981cebdc336ba8fe21e35f29c2e7c
incCool <phr_sdu@163.com>
2019-09-02 17:50:13 +0800

    modify

---
+++ creational_pattern/factory_method.py
@@ -0,0 +1,15 @@
+'''
+Factory Method
+@author : inc
+1.Factory Method
+Define an interface for creating objects, and let the subclass decide which class to instantiate.
+Factory Method delays the instantiation of a class to its subclasses.
+
+Applicability
+When a class doesn't know which class of the object create.
+
+When a class wants to specify the object it creates by its subclasses.
+
+When the class delegates the responsibility of creating an object to one of several help subclasses,
+and you want to localize which help subclass is the agent.
+'''
